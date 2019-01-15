6cd211538f3d994d5d3ea42bee72d322e8bc899e
Elise Phan <aliceconnic@gmail.com>
2019-01-15 04:38:56 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -23,7 +23,8 @@ class Application
         end
       end
       elsif req.path.match(/add/)
-        
+        add_item = req.params["item"]
+        resp.
     else
       resp.write "Path Not Found"
     end
